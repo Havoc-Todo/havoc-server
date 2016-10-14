c7fdb8ede58f8b44133acb80e6fa6bdedb3f9c5b
Justin Joe <jjoe8@gatech.edu>
2016-10-13 21:23:25 -0400

    added data to populate database

---
+++ populate.js
@@ -0,0 +1,22 @@
+const task_arr = [['Finish 4261 project', 'Complete a usable todo list app for android and web', ['Finish backend', 'Finish frontend web', 'Finish frontend Android']],
+    ['Do calculus homework', 'Finish all assigned homework from chapters 1 and 2', ['Finish Chapter 1 - Derivatives', 'Finish Chapter 1 - Integrals', 'Finish Chapter 2 - Graphing']],
+    ['Study for job interviews', 'Study for interview with google', ['Study Algorithms', 'Study data structures', 'Study dynamic programming']],
+    ['Cook meals for the week', 'Cook and freeze lunch and dinner for the upcoming week', ['Cook lunch', 'Freeze lunch', 'Cook dinner', 'Freeze dinner']],
+    ['Do laundry', 'Wash and dry all clothes', ['Wash clothes', 'Dry clothes', 'Fold clothes']],
+    ['Write machine learning essay', 'Write essay on Bayesian networks', ['Write introduction', 'Write body', 'Write Conclusion']],
+    ['Walk dog', 'Walk Dodo around the neighborhood', []]];
+
+const user_arr = ['Joseph', 'Iris', 'Nate', 'Sufyan', 'Justin', 'Mostafa'];
+
+const due_date_arr = [new Date("2016-10-23T10:00:00").getTime(), new Date("2016-10-23T14:00:00").getTime(),
+    new Date("2016-10-24T06:45:00").getTime(), new Date("2016-10-31T20:30:00").getTime(), new Date("2016-11-24T12:00:00").getTime(),
+    new Date("2016-12-25T13:15:30").getTime(), new Date("2016-01-01T00:00:00").getTime()];
+
+const status = {
+    complete:'COMPLETE',
+    incomplete:'INCOMPLETE',
+    archived:'ARCHIVED'
+};
+
+console.log(statusOne.status);
+const status_arr = ['COMPLETE', 'INCOMPLETE'];
