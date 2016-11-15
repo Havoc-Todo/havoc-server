82ae6ca37ca185de054bf17856e2ca1a62751266
Justin <justindjoe@gmail.com>
2016-11-15 14:02:07 -0500

    commented out code

--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,51 @@
 const User = require('../models/user');
 const Task = require('../models/task');
 // const passport = require('../routes/passportSetup');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: "269725695221-9hi2t4e29fkntetkutt13hrn9dk1ebdo.apps.googleusercontent.com",
-    clientSecret: "UbVnBOudP25Yn5K1BUOVsQQ6",
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-       // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-       //   return done(err, user);
-       // });
-    process.nextTick(function(){
-    //   console.log('id : '+ profile.id);
-    //   console.log('name :'+ profile.displayName);
-    //   console.log('email :' + profile.emails);
-    //   console.log('token : '+ token);
-      console.log(profile);
-      return done(null, profile);
-    }); 
+// passport.use(new GoogleStrategy({
+//     clientID: "269725695221-9hi2t4e29fkntetkutt13hrn9dk1ebdo.apps.googleusercontent.com",
+//     clientSecret: "UbVnBOudP25Yn5K1BUOVsQQ6",
+//     callbackURL: "http://localhost:3000/auth/google/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//        //   return done(err, user);
+//        // });
+//     process.nextTick(function(){
+//     //   console.log('id : '+ profile.id);
+//     //   console.log('name :'+ profile.displayName);
+//     //   console.log('email :' + profile.emails);
+//     //   console.log('token : '+ token);
+//       console.log(profile);
+//       return done(null, profile);
+//     }); 
     
-  }
-));
+//   }
+// ));
 
 
 module.exports = [
-  {
-    method: 'GET',
-    path: '/auth/google',
-    handler(request, reply) {
-      passport.authenticate('google', { scope : ['profile'] })
-      console.log("AsDf");
-    }
-  },
-  {
-    method: 'GET',
-     path: '/auth/google/callback',
-    handler(request, reply) {
-      passport.authenticate('google', {successRedirect: '/api/test', failedRedirect: '/api/user'})
-    }
-  },
+  // {
+  //   method: 'GET',
+  //   path: '/auth/google',
+  //   handler(request, reply) {
+  //     passport.authenticate('google', { scope : ['profile'] })
+  //     console.log("AsDf");
+  //   }
+  // },
+  // {
+  //   method: 'GET',
+  //    path: '/auth/google/callback',
+  //   handler(request, reply) {
+  //     passport.authenticate('google', {successRedirect: '/api/test', failedRedirect: '/api/user'})
+  //   }
+  // },
   {
     method: 'POST',
     path: '/auth/login',
     handler(request, reply) {
-      console.log(request.query)
       User.findOne({
         email: request.payload.email,
         password: request.payload.password
@@ -55,6 +54,15 @@ module.exports = [
           if (doc) reply({ status: true, doc })
           else reply({ status: false })
         })
+    }
+  },
+  {
+    method: 'POST',
+    path: '/auth/validate',
+    handler(request, reply) {
+      console.log(request.query)
+      if (request.payload.aud === '269725695221-9hi2t4e29fkntetkutt13hrn9dk1ebdo.apps.googleusercontent.com') reply({status: true})
+      else replay({status: false})
     }
   }
 ]
